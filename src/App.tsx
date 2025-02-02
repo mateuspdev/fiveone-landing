25b48d98aaa18340b198ed259a1a087087e91aa3
mateuspdev <154011109+mateuspdev@users.noreply.github.com>
2025-02-02 20:19:23 -0300

    Change to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 
 import Blog from "./pages/Blog";
 import Home from "./pages/Home";
